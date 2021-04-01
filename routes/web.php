5cc67c295441e880c375b4c99cfb7709f548140b
Saleh Sebah <saleh.sebah.fa@gmail.com>
2021-04-01 12:07:54 +0300

    pass data home work

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function () {
+    $name = request('name');
+    return view('about', compact('name'));
+});
+
+Route::post('/store', function () {
+    $name = request('name');
+    return view('about', compact('name'));
+});
